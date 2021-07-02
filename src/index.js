9d00b490ef05140ed294a93562fa208dfd6fd3d8
samia <samia.benfredj@msaid.de>
2021-07-02 14:44:25 +0200

    fix TodoItem css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./classBased/App.css"
+import "./functionBased/App.css"
 //component file
 import TodoContainer from "./functionBased/components/TodoContainer"
 ReactDOM.render(<TodoContainer />, document.getElementById("root"))
